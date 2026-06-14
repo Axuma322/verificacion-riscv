08b022f7a6bd4dbda4c077f5ede13639cc7a8a2d
Gabriel Porras Salas <144186046+Axuma322@users.noreply.github.com>
2026-06-13 20:57:24 -0600

    prueba

--- Proyecto/tb/testbench.sv
+++ Proyecto/tb/testbench.sv
@@ -1,4 +1,5 @@
-// Define el orden de compilacion del testbench UVM para EDAPlayground.
+// Define el orden de compilacion del testbench UVM para
+//para EDAPlayground.
 `include "ifc_darksocv.sv"
 `include "darksocv_pkg.sv"
 `include "tb_top.sv"
